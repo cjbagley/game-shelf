41eb2fd9e3ef3ea89df1b166b7f1d41f773b7cd5
Colin Bagley <cjbagley@googlemail.com>
2025-01-07 20:43:44 +0000

    feat: Setup basic layout inc. app header

---
+++ resources/js/Components/AppHeader.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <header
+        class="flex h-16 items-center justify-center bg-gray-800 text-xl text-white"
+    >
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+        >
+            <path
+                fill="currentColor"
+                d="M9 3v15h3V3zm3 2l4 13l3-1l-4-13zM5 5v13h3V5zM3 19v2h18v-2z"
+            />
+        </svg>
+        Game Shelf
+    </header>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,20 +1,10 @@
 <script setup lang="ts">
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import { Link } from '@inertiajs/vue3';
+import AppHeader from "@/Components/AppHeader.vue";
 </script>
 
 <template>
-    <div class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900">
-        <div>
-            <Link href="/">
-                <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
-            </Link>
-        </div>
-
-        <div
-            class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg dark:bg-gray-800"
-        >
-            <slot />
-        </div>
+    <AppHeader/>
+    <div class="container mt-5 mx-auto">
+        <slot/>
     </div>
 </template>
